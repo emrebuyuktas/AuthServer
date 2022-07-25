41ad92cd22a0e5982010164d299b4f999eada982
emrebuyuktas <emrebyk348@gmail.com>
2022-07-25 22:52:28 +0300

    Core and shared layers completed

---
+++ AuthServer.Core/Services/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using AuthServer.Core.Dtos;
+using SharedLibrary.Dtos;
+
+namespace AuthServer.Core.Services
+{
+    public interface IUserService
+    {
+        Task<Response<CreateUserDto>> CreateUserAsync(CreateUserDto createUserDto);
+        Task<Response<UserDto>> GetUserByNameAsync(string userName);
+    }
+}
